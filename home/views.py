7474aabacff940d52fdd66978bbfa5ae1f5465e7
noman jazal <nomanjazal@gmail.com>
2022-10-27 20:37:19 +0500

    adding django startup template with template and style

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def Home(request):
+    return render(request, 'index.html')
+
+
+
+
+    
