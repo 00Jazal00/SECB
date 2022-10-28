5c757d740fc9ef961be50352d08ec891a52874c3
noman jazal <nomanjazal@gmail.com>
2022-10-28 20:54:07 +0500

    adding home page and newsletter

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import NEWSLETTER
+
+# admin.site.register(Mian_User)
+
+# Hide model because admin don't need this, Model is used to edit password of user
+def register_hidden_models(*model_names):
+    for m in model_names:
+        ma = type(
+            str(m)+'Admin',
+            (admin.ModelAdmin,),
+            {
+                'get_model_perms': lambda self, request: {}
+            })
+        admin.site.register(m, ma)
+
+register_hidden_models(NEWSLETTER)
--- home/views.py
+++ home/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-
+from .models import NEWSLETTER
 # Create your views here.
 
 def Home(request):
-    return render(request, 'index.html')
+    # Adding news letter
+    news_msg = False
+    if request.method == 'POST':
+        newsletter  = request.POST['newsletter']
+        # Testing if email is already exit
+        find_Newsletter =NEWSLETTER.objects.filter(email=newsletter).first()
+        if find_Newsletter == None:
+            NEWSLETTER(email=newsletter).save()
+            news_msg = True
+
+
+
+    return render(request, 'home.html', {'news_msg':news_msg})
 
 
 
